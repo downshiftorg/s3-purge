169a8d04f274788e3936798aaf04ae971453f953
Jared Henderson <jared@netrivet.com>
2018-01-17 09:39:10 -0500

    netrivet -> downshift

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject s3-purge "0.1.0-SNAPSHOT"
   :description "Destroy things in S3"
-  :url "http://github.com/netrivet/s3-purge"
+  :url "https://github.com/downshiftorg/s3-purge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
